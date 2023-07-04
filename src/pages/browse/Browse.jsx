54a725e28c0a6de87450f9d851f4cce4e95a5025
Lemonyello <84567117+Lemonyello@users.noreply.github.com>
2023-07-04 17:02:58 +0700

    added project initials

---
+++ src/pages/browse/Browse.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Browse() {
+  //05d8a29b030467b18741809bdfe999cc
+  return (
+    <div className="app">
+      <h1>Browse</h1>
+    </div>
+  );
+}
+
+export default Browse;
